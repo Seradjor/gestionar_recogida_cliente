43d509d461ee3c188b019fa0d553a3d3bee2e4ed
Seradjor <serjorad@gmail.com>
2024-04-17 14:10:50 +0000

    Añadido controladores en order.py

--- models/order.py
+++ models/order.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+import datetime
 
 
 class order(models.Model):
@@ -8,9 +11,9 @@ class order(models.Model):
     _description = 'Pedido de cliente'
     _rec_name = 'code'
 
-    code = fields.Char(size = 7, required=True, string="Número")
+    code = fields.Char(size = 7, string="Número", readonly=True, default=lambda self: self._generate_order_number())
     state = fields.Selection([('0','Iniciado'),('1','Realizado'),('2','Preparado'),('3','Confirmada recogida'),('4','Recogido')],default = '0', required=True, string="Estado")
-    order_date = fields.Date(string="Fecha pedido")
+    order_date = fields.Date(string="Fecha pedido", default=lambda self:datetime.date.today())
     pick_up_date = fields.Date(string="Fecha recogida")
 
     # Enlace con client
@@ -21,6 +24,30 @@ class order(models.Model):
     # Enlace con product a través de order_product
     products_ids = fields.One2many('gestionar_recogida_cliente.order_product', 'order_id', string="Productos")
 
+
+    # Generamos número de pedido automáticamente
+    def _generate_order_number(self):
+        today = datetime.date.today()
+        year = str(today.year)
+        last_order = self.search([], order='id desc', limit=1)
+        if last_order:
+            last_code = last_order.code
+            last_number = int(last_code[4:])  # Excluyendo la longitud del año, obtenemos los últimos 3 dígitos del código.
+            new_number = last_number + 1
+            new_code = year + str(new_number).zfill(3)  # Rellenar con ceros a la izquierda
+        else:
+            new_code = year + '001'
+        return new_code
+    
+    # Comprobación fecha recogiga es posterior a fecha pedido
+    @api.constrains('pick_up_date')
+    def _check_pick_up_date(self):
+        for record in self:
+            if record.pick_up_date < record.order_date:
+                raise ValidationError('La fecha de recogida no puede ser anterior a la fecha de pedido.')
+
+
+    """ FUNCIONES BOTONERAS """
 
     # Función confirmar pedido
     def state_1(self):
